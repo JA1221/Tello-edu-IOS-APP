db3bf011ddda8178fce7fd1de50e339324a0bb7e
LU, CHUN-AN <cat605101@yahoo.com.tw>
2019-06-19 15:38:54 +0800

    add file

--- Tello/ViewController.swift
+++ Tello/ViewController.swift
@@ -1,20 +1,51 @@
-//
-//  ViewController.swift
-//  Tello
-//
-//  Created by 李岱米 on 2019/6/15.
-//  Copyright © 2019 JA1221. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var sendData: UITextField!
+    @IBOutlet weak var receiveData: UILabel!
+    
+    //設定 IP Port , 指定UDP連線
+    let host = "192.168.10.1"
+    let port = 8889
+    var client: UDPClient?
+    var server: UDPServer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        client = UDPClient(address: host, port: Int32(port))//建立 UDP 連線
+        server = UDPServer(address: "0.0.0.0", port: 8890)
     }
 
-
+    @IBAction func onClick(_ sender: Any) {
+        //guard let client = client else { return }
+        
+        if let text = sendData.text{
+            send( text)//傳送 textView文字指令
+            sendData.text = ""//清空
+            let s = server?.recv(5)
+            print(s!.0)
+            print(s!.1)
+            print(s!.2)
+        }
+    }
+    @IBAction func takeoff(_ sender: Any) {
+        send("command")
+        send("takeoff")
+    }
+    
+    @IBAction func land(_ sender: Any) {
+        send("land")
+    }
+    
+    @IBAction func flipF(_ sender: Any) {
+        send("flip f");
+    }
+    
+    func send(_ s: String){
+        guard let client = client else {return}
+        _ = client.send(string: s)
+    }
 }
 
