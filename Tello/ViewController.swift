5febb3d34cd0bcf2d86e7b110d3470393ea01be8
LU, CHUN-AN <cat605101@yahoo.com.tw>
2019-06-20 23:38:01 +0800

    update
    recv Array [byte]轉 string

--- Tello/ViewController.swift
+++ Tello/ViewController.swift
@@ -5,21 +5,24 @@ class ViewController: UIViewController {
     @IBOutlet weak var sendData: UITextField!
     @IBOutlet weak var receiveData: UILabel!
     
-    let serverIP = "192.168"
+    let serverIP = "192.168.43.36"
     let serverPort = 8787
     //設定 IP Port , 指定UDP連線
     let host = "192.168.43.103"
     let port = 8889
     var client: UDPClient?
     var server: UDPServer?
+    var data = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         client = UDPClient(address: host, port: Int32(port),myAddresss: serverIP, myPort: Int32(serverPort))//建立 UDP 連線
         server = UDPServer(address: "0.0.0.0", port: 8890)
-        
         readData()
+//        DispatchQueue.main.async {
+//            self.receiveData.text = self.data
+//        }
     }
 
     @IBAction func onClick(_ sender: Any) {
@@ -28,10 +31,6 @@ class ViewController: UIViewController {
         if let text = sendData.text{
             send( text)//傳送 textView文字指令
             sendData.text = ""//清空
-            let s = client.recv(10)
-            print(s.0)
-            print(s.1)
-            print(s.2)
         }
     }
     @IBAction func takeoff(_ sender: Any) {
@@ -51,6 +50,9 @@ class ViewController: UIViewController {
         guard let client = client else {return}
         _ = client.send(string: s)
     }
+    @IBAction func emergency(_ sender: Any) {
+        send("emergency")
+    }
     
     func readData(){
         let queue = DispatchQueue(label: "com.nkust.JA1221")
@@ -60,14 +62,23 @@ class ViewController: UIViewController {
             {
                 guard let client = self.client else { return }
                 
-                print("test")
-                self.receiveData.text = self.receiveData.text ?? "0" + "1"
-                let s = client.recv(10)
+                let s = client.recv(20)
+                self.data = self.get_String_Data(s.0!)
+                print(self.data)
+                DispatchQueue.main.async {
+                    self.receiveData.text = self.data
+                }
                 print(s.0)
                 print(s.1)
                 print(s.2)
             }
+            
         }
+    }
+    
+    func get_String_Data(_ data: [Byte]) -> (String){
+        let string1 = String(data: Data(data), encoding: .utf8) ?? ""
+        return string1
     }
 }
 
